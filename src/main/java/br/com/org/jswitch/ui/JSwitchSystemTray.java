77a11f3cafd10d9b69cb4bde7ccc1b226a052f49
Anderson Silveira <anderson.silveira.br@gmail.com>
2014-11-01 10:50:51 -0200

    hide popupmenu when mouseclick outside

--- src/main/java/br/com/org/jswitch/ui/JSwitchSystemTray.java
+++ src/main/java/br/com/org/jswitch/ui/JSwitchSystemTray.java
@@ -8,12 +8,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.io.File;
 import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JDialog;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.UIManager;
@@ -33,8 +36,12 @@ public class JSwitchSystemTray implements FileChangeListener {
     private OperationSystemManager systemManager;
     private TrayIcon icon;
     private CheckboxMenuItemGroupListener menuItemListener;
+    /* Added a "hidden dialog" */
+    private JDialog hiddenDialog;
 
     public void show() throws Exception {
+	
+	
 
 	systemManager = new OperationSystemManager();
 	if (!SystemTray.isSupported()) {
@@ -55,11 +62,21 @@ public class JSwitchSystemTray implements FileChangeListener {
 	List<String> jdkInstalled = systemManager.getJDKInstalled();
 	ButtonGroup buttonGroup = new ButtonGroup();
 	menuItemListener = new CheckboxMenuItemGroupListener(systemManager, icon);
+	
+	  ActionListener listener = new ActionListener () {
+	        @Override
+	        public void actionPerformed (ActionEvent ae) {
+	            /* We want to make sure the hidden dialog goes away after selection */
+	            hiddenDialog.setVisible(false);
+	        }
+	    };
+	    
 	for (final String jdk : jdkInstalled) {
 	    JCheckBoxMenuItem checkboxMenuItem = new JCheckBoxMenuItem(jdk);
 	    buttonGroup.add(checkboxMenuItem);
 	    jpopup.add(checkboxMenuItem);
 	    menuItemListener.add(checkboxMenuItem);
+	    checkboxMenuItem.addActionListener(listener);
 
 	}
 	jpopup.addSeparator();
@@ -72,19 +89,37 @@ public class JSwitchSystemTray implements FileChangeListener {
 	});
 	icon.addMouseListener(new MouseAdapter() {
 	    @Override
-	    public void mouseClicked(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 1) {
-		    jpopup.setInvoker(jpopup);
-		    jpopup.setVisible(true);
-		    jpopup.setLocation((e.getX() - jpopup.getWidth()), (e.getY() - jpopup.getHeight()));
-
-		}
+	    public void mouseClicked(MouseEvent me) {
+		if (me.getButton() == MouseEvent.BUTTON1 && me.getClickCount() == 1) {
+                    /* Place the hidden dialog at the same location */
+                    hiddenDialog.setLocation(me.getX(), me.getY());
+                    /* Now the popup menu's invoker is the hidden dialog */
+                    jpopup.setInvoker(hiddenDialog);
+                    hiddenDialog.setVisible(true);
+                    jpopup.setVisible(true);
+                    jpopup.setLocation((me.getX() - jpopup.getWidth()), (me.getY() - jpopup.getHeight()));
+		
 	    }
-	});
+	   }
 
+	});
+	
 	jpopup.add(closeItem);
 
 	tray.add(icon);
+	/* Initialize the hidden dialog as a headless, titleless dialog window */
+	hiddenDialog = new JDialog ();
+	hiddenDialog.setSize(10, 10);
+	/* Add the window focus listener to the hidden dialog */
+	hiddenDialog.addWindowFocusListener(new WindowFocusListener () {
+	    @Override
+	    public void windowLostFocus (WindowEvent we ) {
+		hiddenDialog.setVisible(false);
+	    }
+	    @Override
+	    public void windowGainedFocus (WindowEvent we) {}
+	});
+	
 	icon.displayMessage("JSwitch", "Foi iniciado com sucesso!!", TrayIcon.MessageType.INFO);
 
 	FileMonitor fileMonitor = FileMonitor.getInstance();
@@ -95,7 +130,7 @@ public class JSwitchSystemTray implements FileChangeListener {
     private ImageIcon createIconClose() {
 	ImageIcon iconClose = new ImageIcon((getClass().getResource("/logout.png")));
 	Image imageOrg = iconClose.getImage(); // transform it
-	Image imageNew = imageOrg.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH); 
+	Image imageNew = imageOrg.getScaledInstance(16, 16, java.awt.Image.SCALE_SMOOTH); 
 	iconClose = new ImageIcon(imageNew);
 	return iconClose;
     }
@@ -112,4 +147,5 @@ public class JSwitchSystemTray implements FileChangeListener {
 	}
 
     }
+    
 }
