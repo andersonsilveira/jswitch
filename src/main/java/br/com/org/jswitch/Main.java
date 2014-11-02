623df273cb6efbc16f35079a05a955b357032c7c
Anderson Silveira <anderson.silveira.br@gmail.com>
2014-11-02 04:15:44 -0200

    set java_home default when init systray

--- src/main/java/br/com/org/jswitch/Main.java
+++ src/main/java/br/com/org/jswitch/Main.java
@@ -5,6 +5,7 @@ package br.com.org.jswitch;
 
 import javax.swing.JOptionPane;
 
+import br.com.org.jswitch.cfg.exception.JavaHomeVariableSystemNotFoundException;
 import br.com.org.jswitch.control.OperationSystemManager;
 import br.com.org.jswitch.ui.JSwitchSystemTray;
 import br.com.org.jswitch.ui.JSwitchUI;
@@ -30,7 +31,14 @@ public class Main {
 			}else{
 				new JSwitchSystemTray().show();				
 			}
-		} catch (Exception e) {
+		} 
+		catch (JavaHomeVariableSystemNotFoundException e) {
+			JOptionPane.showMessageDialog(null,
+					"O Systema operacional não esta com a variável JAVA_HOME configurada, por favor faça a configuração antes de iniciar o aplicativo",
+					"JSwitch", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		catch (Exception e) {
 			JOptionPane.showMessageDialog(null,
 					"O JSwicht ocorreu um erro durante a inicialização do aplicativo",
 					"JSwitch", JOptionPane.ERROR_MESSAGE);
